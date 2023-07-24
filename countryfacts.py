92215ced097e83431b3c22fb8c94216e817c7c6f
JesseXC <100256437+JesseXC@users.noreply.github.com>
2023-07-24 13:36:39 -0700

    Update countryfacts.py

--- countryfacts.py
+++ countryfacts.py
@@ -1,13 +1,11 @@
 from countryinfo import CountryInfo
 import difflib
 import sqlite3
-import pandas as pd 
+import pandas as pd
 from seoproject import added_to_database
 
-
-
 list_of_names = []
-list_of_langs=[]
+list_of_langs = []
 countryname = CountryInfo("nigeria")
 acc = 1
 
@@ -16,57 +14,45 @@ for name in countryname.all():
     acc += 1
 
 def search_by_country_name(nameeofcount):
+    try:
         countryAPI = CountryInfo(nameeofcount)
-        results = [] 
+    except KeyError:
+        return None
 
-        # Create object
-        x = {
-            'Name': [],
-            #'Alt Spellings': [],
-            'Region': [],
-            'Subregion': [],
-            'Capital': [],
-            #'Currency': [],
-            #'Languages': [],
-            #'Provinces': [],
-            #'Timezones': []
-        }
-        # Fill object
+    results = []
 
-        results.append(("Name:", nameeofcount))
-        x['Name'].append(nameeofcount)
-        results.append(("Alt Spellings:", countryAPI.alt_spellings()))
-        #x['Alt Spellings'].append(countryAPI.alt_spellings())
-        results.append(("Region:", countryAPI.region()))
-        x['Region'].append(countryAPI.region())
-        results.append(("Subregion:", countryAPI.subregion()))
-        x['Subregion'].append(countryAPI.subregion())
-        results.append(("Capital:", countryAPI.capital()))
-        x['Capital'].append(countryAPI.capital())
+    x = {
+        'Name': [],
+        'Region': [],
+        'Subregion': [],
+        'Capital': [],
+        'Currency': [],
+        'Languages': [],
+        'Provinces': [],
+        'Timezones': []
+    }
 
-        if len(countryAPI.currencies()) == 1:
-            results.append(("Currency:", countryAPI.currencies()[0]))
-        else:
-            results.append(("Currencies:", countryAPI.currencies()))
-        #x['Currency'].append(countryAPI.currencies())
+    results.append(("Name:", nameeofcount))
+    x['Name'].append(nameeofcount)
+    results.append(("Alt Spellings:", countryAPI.alt_spellings()))
+    results.append(("Region:", countryAPI.region()))
+    x['Region'].append(countryAPI.region())
+    results.append(("Subregion:", countryAPI.subregion()))
+    x['Subregion'].append(countryAPI.subregion())
+    results.append(("Capital:", countryAPI.capital()))
+    x['Capital'].append(countryAPI.capital())
 
-        results.append(("Languages:", countryAPI.languages()))
-        #x['Languages'].append(countryAPI.languages())
-        results.append(("Provinces:", countryAPI.provinces()))
-        #x['Provinces'].append(countryAPI.provinces())
-        results.append(("Timezones:", countryAPI.timezones()))
-        #x['Timezones'].append(countryAPI.timezones())
-        # result_string = "\n".join([f"{key} {value}" for key, value in results])
+    if len(countryAPI.currencies()) == 1:
+        results.append(("Currency:", countryAPI.currencies()[0]))
+    else:
+        results.append(("Currencies:", countryAPI.currencies()))
 
-        #print(pd.DataFrame(results))
+    results.append(("Languages:", countryAPI.languages()))
+    results.append(("Provinces:", countryAPI.provinces()))
+    results.append(("Timezones:", countryAPI.timezones()))
 
-        # Send object to database
-        #df = pd.DataFrame.from_dict(x)
-        # print(df)
-        
-        # added_to_database(pd.DataFrame.from_dict(x))
-        return results
-    
+    return results
+
 
 def search_country(search_option, list_of_names):
     search_option = search_option.lower()
@@ -78,16 +64,25 @@ def search_country(search_option, list_of_names):
         similar_matches = difflib.get_close_matches(search_option, list_of_names)
         if len(similar_matches) > 0:
             print("Country not found. Did you mean one of these?")
-            for match in similar_matches:
-                print(match)
+            for idx, match in enumerate(similar_matches, 1):
+                print(f"{idx}. {match}")
+
+            choice = input("Choose the number corresponding to the correct country or 'q' to quit: ")
+            if choice == 'q':
+                return None
+            elif choice.isdigit() and 1 <= int(choice) <= len(similar_matches):
+                search_option = similar_matches[int(choice) - 1]
+            else:
+                print("Invalid choice. Please try again.")
         else:
             print("Country does not exist, I'm sorry")
+            return None
 
-# printhistory=input("Would you like to see previous Countries Searched?(Y or N): ")
-# def history(printhistory):
-#     if printhistory.lower() == "yes" or printhistory.lower() == "y":
-#         added_to_database(data_frame)
-#         print("Have a nice day!")
-#     else:
-#         print("Ok, Have a nice day!")
-# history(printhistory)
+
+        return search_country(search_option, list_of_names)
+
+''' So what the program does is that it uses a try and except block to catch the KeyError when attempting to retrieve information for a non-existent country. 
+The function search_by_country_name returns None if the country does not exist. In the search_country function, 
+if the country doesn't exist, it prints a message and returns None, effectively terminating the search. Additionally, 
+when similar matches are presented, the function calls itself recursively to allow the user to try again with the new search_option. 
+This way, the user can retry their search without having to restart the entire program.'''
